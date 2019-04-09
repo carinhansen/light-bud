8534dd1c09764a694d72e5fe28913b7d5ba3474a
Arno de Jong <arno@elephantcs.nl>
2019-04-09 15:53:26 +0200

    Adds styling for the map and the first flightplan

--- app.js
+++ app.js
@@ -1,25 +1,238 @@
 let map;
 
 function initMap() {
+
+  var styledMapType = new google.maps.StyledMapType(
+    [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dadada"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c9c9c9"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      }
+    ],
+
+    {name: 'Styled Map'});
+
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
-    center: {lat: 51.9181097, lng: 4.4876019},
+    center: {lat: 51.9170588, lng: 4.484828},
     mapTypeId: 'terrain'
   });
 
   let flightPlanCoordinates = [
+    {lat: 51.917918, lng: 4.487792},
+    {lat: 51.917421, lng: 4.488038},
+    {lat: 51.917698, lng: 4.489387},
+    {lat: 51.917421, lng: 4.488038},
+    {lat: 51.916793, lng: 4.485435},
+    {lat: 51.916160, lng: 4.485735},
+    {lat: 51.916793, lng: 4.485435},
+    {lat: 51.916883, lng: 4.485394},
+    {lat: 51.916480, lng: 4.483603},
+    {lat: 51.916883, lng: 4.485394},
+    {lat: 51.917821, lng: 4.484882},
+    {lat: 51.917579, lng: 4.483653},
+    {lat: 51.917159, lng: 4.483827},
     {lat: 51.917122, lng: 4.483987},
     {lat: 51.918208, lng: 4.489325},
-    // {lat: -18.142, lng: 178.431},
-    // {lat: -27.467, lng: 153.027}
   ];
   let flightPath = new google.maps.Polyline({
     path: flightPlanCoordinates,
     geodesic: true,
     strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
+    strokeOpacity: 0.5,
+    strokeWeight: 10
   });
+
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
 
   flightPath.setMap(map);
 }
